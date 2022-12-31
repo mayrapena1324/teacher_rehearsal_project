fea5e01e0c48eda27f6e00d33c665b8088a500e0
Mayra Pena(Peña) <97967943+mayrapena1324@users.noreply.github.com>
2022-12-31 12:10:27 -0600

    Add login Manager

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 
 
 # configure flask
@@ -12,6 +13,24 @@ Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///rehearsify.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+# CONFIGURE FLASK_LOGIN
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+##  Configure Tables
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250))
+    name = db.Column(db.String(250), nullable=False)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
