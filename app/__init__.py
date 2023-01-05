1457fec966ed6f5ef63686b82b3116dfa3f93f20
Mayra Pena(Peña) <97967943+mayrapena1324@users.noreply.github.com>
2023-01-05 15:38:27 -0600

    Configuring the flask factory and adding db connection.

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,26 @@
 # make our app a package
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-if app.config["DEBUG"]:
-    app.config.from_object("config.DevelopmentConfig")
 
-else:
-    app.config.from_object("config.Config")
+def create_app():
+    app = Flask(__name__)
+
+    if app.config["DEBUG"]:
+        app.config.from_object("config.DevelopmentConfig")
+
+    else:
+        app.config.from_object("config.Config")
+
+    db.init_app(app)
+
+    with app.app_context():
+        from . import user_views, public_views  # Import routes
+        db.create_all()  # Create sql tables for our data models
+
+        return app
 
 # importing views files to avoid a circular import
 from app import public_views
