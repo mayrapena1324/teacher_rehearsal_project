7a6e21875b35e63be20f5cdf6b9f29670b7b51a3
Mayra Pena(Peña) <97967943+mayrapena1324@users.noreply.github.com>
2023-01-07 16:23:27 -0600

    Add Card body with Nav tabs. Restructure Rehearsal page to only have card with tabs.

--- app/user_views.py
+++ app/user_views.py
@@ -1,5 +1,5 @@
 from flask_login import logout_user, current_user
-from flask import render_template, url_for, redirect, flash
+from flask import render_template, url_for, redirect, flash, request
 from flask import current_app as app
 from .models import db, Rehearsal
 
@@ -16,5 +16,10 @@ def rehearsal():
         flash("You need to login or register to start rehearsing.")
         return redirect(url_for("login"))
     form = Rehearsal()
+    if request.method == "POST":
+        # The text box needs to display anything in the database for that lesson
+        # create an edit functionality - Text editor will come up
+
+        return redirect(request.url)
     return render_template("user/rehearsal.html", form=form, current_user=current_user,
                            logged_in=current_user.is_authenticated)
